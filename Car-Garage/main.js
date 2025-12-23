91e055958758de5445fe83ff27053adb297338ed
moumen <moumen@vbox.myguest.virtualbox.org>
2025-12-23 12:51:41 +0300

    finished the Car-garage

--- Car-Garage/main.js
+++ Car-Garage/main.js
@@ -2,6 +2,7 @@ const DisplayArea = document.getElementById('displayCars');
 const inputBrand = document.getElementById('brand');
 const inputYear = document.getElementById('year');
 const inputColor = document.getElementById('color');
+const submitBtn =document.getElementById('submit')
 const garage = [];
 
 
@@ -13,7 +14,36 @@ function Car(brand, year, color){
 }
 
 Car.prototype.getName=function() {
-  return this.brand+" "+this.prodYear+"-"+this.color;
+  return this.brand+" "+"            "+ this.prodYear+"-"+this.color;
+}
+
+function DisplayCars(){
+  DisplayArea.textContent="";
+  
+  garage.forEach((car, index) => {
+    const carToDisplay = document.createElement('div');
+    carToDisplay.textContent=car.getName();
+
+    const delBtn = document.createElement("button");
+    delBtn.textContent="Delete";
+    delBtn.addEventListener('click',  ()=> {
+
+    garage.splice(index,1)
+    DisplayCars();
+    })
+    carToDisplay.appendChild(delBtn);
+
+    DisplayArea.appendChild(carToDisplay);
+    
+
+  })
+
+}
+
+function clearInputs() {
+  inputBrand.value = "";
+  inputYear.value = "";
+  inputColor.value = "";
 }
 
 function addCarsFromInput(){
@@ -32,5 +62,6 @@ function addCarsFromInput(){
   const car = new Car(brand, year, color);
   garage.push(car);
   DisplayCars();
-  ClearInput();
+  clearInput();
 }
+  submitBtn.addEventListener('click', ()=> addCarsFromInput());
